049c5f62a3541e1724da2982818d4ee7a57f9994
unknown <blairbowen35@gmail.com>
2023-11-11 11:09:02 -0500

    still some stdout that I don't like

--- MessageBoardClient.java
+++ MessageBoardClient.java
@@ -1,51 +1,52 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.*;
 
-public class MessageBoardClient
-{
-    public static void main(String[] args)
-    {
+public class MessageBoardClient {
+    public static void main(String[] args) {
         String serverAddress = "localhost";
         int serverPort = 42069; // nice
 
         try (Socket socket = new Socket(serverAddress, serverPort);
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             Scanner scanner = new Scanner(System.in))
-        {
+             Scanner scanner = new Scanner(System.in)) {
 
             System.out.print("Enter your username: ");
             String username = scanner.nextLine();
             out.println(username);
 
-            System.out.println("You are now connected to the message board. Type messages below:");
+            System.out.println("You are now connected to the message board.");
+            System.out.println("Type 'GET' to retrieve a message by ID or type your message subject below:");
 
             Thread receiveThread = new Thread(() -> {
-                try
-                {
+                try {
                     String message;
-                    while ((message = in.readLine()) != null)
-                    {
+                    while ((message = in.readLine()) != null) {
                         System.out.println(message);
                     }
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
             receiveThread.start();
 
-            String message;
-            while (true)
-            {
-                message = scanner.nextLine();
-                out.println(message);
+            while (true) {
+                String line = scanner.nextLine();
+                if (line.equalsIgnoreCase("GET")) {
+                    System.out.print("Enter message ID: ");
+                    String id = scanner.nextLine();
+                    out.println("GET_MESSAGE:" + id);
+                    System.out.println("Type 'GET' to retrieve a message by ID or type your message subject below:");
+                } else {
+                    out.println(line); // Send subject
+                    System.out.println("Enter your message:");
+                    String content = scanner.nextLine();
+                    out.println(content); // Send content
+                    System.out.println("Type 'GET' to retrieve a message by ID or type your message subject below:");
+                }
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- MessageBoardServer.java
+++ MessageBoardServer.java
@@ -1,96 +1,112 @@
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.text.SimpleDateFormat;
 
-public final class MessageBoardServer
-{
+public final class MessageBoardServer {
     private static final ArrayList<PrintWriter> clientWriters = new ArrayList<>();
+    private static final Map<Integer, Message> messages = new HashMap<>();
+    private static int messageID = 0;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int serverPort = 42069; // nice
         
-        try (ServerSocket serverSocket = new ServerSocket(serverPort))
-        {
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
             System.out.println("Message Board Server started on port " + serverPort);
             
-            while (true)
-            {
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
 
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 clientWriters.add(out);
-                // new ClientHandler(clientSocket).start();
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    static class ClientHandler extends Thread
-    {
+    static class ClientHandler extends Thread {
         private final Socket clientSocket;
         private final PrintWriter out;
         private String username;
 
         public ClientHandler(Socket socket) {
             this.clientSocket = socket;
-            try
-            {
+            try {
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException("Error creating client writer.");
             }
         }
 
         public void run() {
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
-            {
-
-                // Handle the client connection
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 username = in.readLine();
                 System.out.println("User '" + username + "' connected.");
 
-                String message;
-                while ((message = in.readLine()) != null)
-                {
-                    System.out.println(username + ": " + message);
-                    // Broadcast the message to all connected clients
-                    broadcastMessage(username + ": " + message);
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (line.startsWith("GET_MESSAGE:")) {
+                        int requestedID = Integer.parseInt(line.split(":")[1]);
+                        Message requestedMessage = messages.get(requestedID);
+                        if (requestedMessage != null) {
+                            out.println(requestedMessage);
+                        } else {
+                            out.println("Message not found");
+                        }
+                    } else {
+                        String subject = line;
+                        String content = in.readLine();
+                        Message message = new Message(++messageID, username, subject, content);
+                        messages.put(messageID, message);
+                        broadcastMessage(message.getDisplayString());
+                    }
                 }
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
-            }
-            finally
-            {
-                try
-                {
+            } finally {
+                try {
                     clientSocket.close();
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
                 clientWriters.remove(out);
             }
         }
 
-        private void broadcastMessage(String message)
-        {
-            for (PrintWriter client : clientWriters)
-            {
+        private void broadcastMessage(String message) {
+            for (PrintWriter client : clientWriters) {
                 client.println(message);
             }
+        }
+    }
+
+    static class Message {
+        int id;
+        String sender;
+        String date;
+        String subject;
+        String content;
+
+        public Message(int id, String sender, String subject, String content) {
+            this.id = id;
+            this.sender = sender;
+            this.subject = subject;
+            this.content = content;
+            this.date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        }
+
+        public String getDisplayString() {
+            return id + ", " + sender + ", " + date + ", " + subject;
+        }
+
+        @Override
+        public String toString() {
+            return "Content: " + content;
         }
     }
 }
